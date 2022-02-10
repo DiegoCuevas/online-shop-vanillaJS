253b138b3cabfdd7b845c56b6f9a46da592f9094
Diego Cuevas <diegocuevas579@gmail.com>
2022-02-10 11:59:34 -0500

    ADD: total price

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function createCartProduct(product) {
       <div class="text-center">${product.name}</div>
     </div>
     <div class="flex w-1/2 justify-between px-4 py-2">
-    <div class="w-2/3 text-center py-2">$${product.price || 0}</div>
+    <div class="w-2/3 text-center py-2">$${(product.price || 0) * product.count}</div>
     <div class="w-1/3 text-center py-2">${product.count}</div>
       <button onclick="removeProduct(${product.id})" 
         class="rounded px-3 flex items-center justify-center bg-gray-600 rounded-full">
